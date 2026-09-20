a1a91d18240357ff778f907a7fee9a374da54d66
sinhaadityacs-oss <sinha.aditya.cs@gmail.com>
2026-09-20 14:09:05 +0530

    Add mobile menu functionality

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const menuButton = document.getElementById('menuButton')
+const mobileMenu = document.getElementById('mobileMenu')
+
+
+menuButton.addEventListener('click', () => {
+
+    mobileMenu.classList.toggle('active')
+
+})
